364d67a6372be6b023e48bfa87d866f905948137
porinky0424 <daichi5967@gmail.com>
2023-08-15 17:03:55 +0900

    チュートリアルdone

--- src/fastapi_tutorial/main.py
+++ src/fastapi_tutorial/main.py
@@ -1,24 +1,8 @@
-from datetime import datetime
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
 
-fake_db = {}
+app = FastAPI(openapi_url="/api/v1/openapi.json")
 
 
-class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: Union[str, None] = None
-
-
-app = FastAPI()
-
-
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
-    print(fake_db)
+@app.get("/items/")
+async def read_items():
+    return [{"name": "Foo"}]
