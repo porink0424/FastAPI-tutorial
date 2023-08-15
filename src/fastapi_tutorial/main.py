22b7b60ddb74628bf67230a1acd0552a474800ea
porinky0424 <daichi5967@gmail.com>
2023-08-15 15:04:30 +0900

    JSON Compatible Encoderまでdone

--- src/fastapi_tutorial/main.py
+++ src/fastapi_tutorial/main.py
@@ -1,25 +1,24 @@
-from typing import Annotated
+from datetime import datetime
+from typing import Union
 
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+fake_db = {}
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Union[str, None] = None
+
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(
-        gt=0, description="The price must be greater than zero"
-    )
-    tax: float | None = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: int, item: Annotated[Item, Body(embed=True)]
-) -> dict:
-    results = {"item_id": item_id, "item": item}
-    return results
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
+    print(fake_db)
