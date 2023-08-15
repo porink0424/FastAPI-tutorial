174eef65ceb5bc71e88ec260c36c156a1c5a835c
porinky0424 <daichi5967@gmail.com>
2023-08-15 11:32:09 +0900

    Body - Multiple Parametersまでdone

--- src/fastapi_tutorial/main.py
+++ src/fastapi_tutorial/main.py
@@ -1,18 +1,19 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Query
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    item_id: Annotated[
-        int, Path(title="The ID of the item to get", ge=1, deprecated=True)
-    ],
-    q: str,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
     return results
